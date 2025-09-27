4f59d2d21d452243d5ae68ab9bd2995c638db664
a Cat with schizophrenia <70656855+overcatoff@users.noreply.github.com>
2025-09-27 13:42:50 +0200

    Updated src.py
    just changed some small things and made the !setrole command actually set a ping

--- src.py
+++ src.py
@@ -318,7 +318,7 @@ async def notify_new_run(session, run, game_name):
         embed = discord.Embed(
             title=f"🚨 New {game_name} Speedrun Needs Verification!",
             url=weblink,
-            description=f"A new run for **{game_name}** was submitted and is awaiting verification.",
+            description=f"A new run for **{game_name}** was submitted and is awaiting verification. <@&{ROLE_ID}>",
             color=discord.Color.red()
         )
         embed.add_field(name="🏃 Runner", value=runner, inline=True)
